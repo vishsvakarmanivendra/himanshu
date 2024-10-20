ac4f8f7d43628913f6a62767a0158b299ca3b284
vishsvakarmanivendra <vishvakarmanivendra@gmail.com>
2024-10-20 08:08:25 +0530

    connect db

--- db/dbconection.js
+++ db/dbconection.js
@@ -1,13 +1,32 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv"
+import mysql2 from "mysql2"
+dotenv.config()
+
+console.log(process.env.DATABASE_PASSWORD,process.env.DATABASE_USER)
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+    dialect: 'mysql',
+    logging: false,
+  });
+sequelize.authenticate().then(result=>{
+    console.log(result);
+}).catch(err=>{
+    console.log(err);
+});
+
+// export default sequelize;
+
 // import { Sequelize } from "sequelize";
 // import dotenv from "dotenv"
 // import mysql2 from "mysql2"
 // dotenv.config()
 
-// console.log(process.env.DATABASE_PASSWORD,process.env.DATABASE_USER)
-// const sequelize = new Sequelize(process.env.MYSQL_URL, {
+// const sequelize = new Sequelize("himanshu",process.env.DATABASE_USER,process.env.DATABASE_PASSWORD, {
+//     host: "localhost",
 //     dialect: 'mysql',
-//     logging: false,
-//   });
+//     dialectModule: mysql2
+// });
+
 // sequelize.authenticate().then(result=>{
 //     console.log(result);
 // }).catch(err=>{
@@ -15,25 +34,6 @@
 // });
 
 // export default sequelize;
-
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv"
-import mysql2 from "mysql2"
-dotenv.config()
-
-const sequelize = new Sequelize("himanshu",process.env.DATABASE_USER,process.env.DATABASE_PASSWORD, {
-    host: "localhost",
-    dialect: 'mysql',
-    dialectModule: mysql2
-});
-
-sequelize.authenticate().then(result=>{
-    console.log(result);
-}).catch(err=>{
-    console.log(err);
-});
-
-export default sequelize;
 
 
 
