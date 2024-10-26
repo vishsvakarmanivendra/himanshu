343323f36770cc271fb158d95d002c2ff1bbc0b5
vishsvakarmanivendra <vishvakarmanivendra@gmail.com>
2024-10-26 20:29:59 +0530

    latest26

--- db/dbconection.js
+++ db/dbconection.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv"
 import mysql2 from "mysql2"
 dotenv.config()
 
-console.log(process.env.DATABASE_PASSWORD,process.env.DATABASE_USER)
 const sequelize = new Sequelize(process.env.MYSQL_URL, {
     dialect: 'mysql',
     logging: false,
